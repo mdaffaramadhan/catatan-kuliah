2c33b7e7faf75b0749e45049362b0c55c5d48c3d
Muhammad Daffa Ramadhan <amateurmdr@gmail.com>
2025-06-03 18:31:10 +0700

    Menambahkan fitur login menggunaka GOogle API

--- app/src/main/java/com/daffa0049/catatankuliah/data/model/User.kt
+++ app/src/main/java/com/daffa0049/catatankuliah/data/model/User.kt
@@ -1,8 +1,7 @@
 package com.daffa0049.catatankuliah.data.model
 
 data class User(
-    val id: Int,
-    val name: String,
-    val email: String,
-    val photoUrl: String
+    val name : String = "",
+    val email : String = "",
+    val photoUrl : String = ""
 )
--- app/src/main/java/com/daffa0049/catatankuliah/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/daffa0049/catatankuliah/ui/screen/HomeScreen.kt
@@ -3,6 +3,7 @@ package com.daffa0049.catatankuliah.ui.screen
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -15,18 +16,15 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -36,32 +34,55 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.key
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.credentials.ClearCredentialStateRequest
+import androidx.credentials.CredentialManager
+import androidx.credentials.CustomCredential
+import androidx.credentials.GetCredentialRequest
+import androidx.credentials.GetCredentialResponse
+import androidx.credentials.exceptions.ClearCredentialException
+import androidx.credentials.exceptions.GetCredentialException
 import coil.ImageLoader
 import coil.compose.rememberAsyncImagePainter
+import com.daffa0049.catatankuliah.BuildConfig
+import com.daffa0049.catatankuliah.R
 import com.daffa0049.catatankuliah.data.model.Note
+import com.daffa0049.catatankuliah.data.model.User
+import com.daffa0049.catatankuliah.data.network.UserDataStore
+import com.google.android.libraries.identity.googleid.GetGoogleIdOption
+import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
+import com.google.android.libraries.identity.googleid.GoogleIdTokenParsingException
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
     viewModel: HomeViewModel,
     onAddNote: () -> Unit,
-    onEditNote: (com.daffa0049.catatankuliah.data.model.Note) -> Unit
+    onEditNote: (Note) -> Unit
 ) {
     val context = LocalContext.current
+    val dataStore = UserDataStore(context)
+    val user by dataStore.userFlow.collectAsState(User())
     val isConnected = isNetworkAvailable(context)
     val notes by viewModel.notes.collectAsState()
     val isLoading by viewModel.isLoading.collectAsState()
     val errorMessage by viewModel.errorMessage.collectAsState()
-    val imageLoader = ImageLoader.Builder(context)
+    ImageLoader.Builder(context)
         .crossfade(true)
         .respectCacheHeaders(false)
         .build()
+    var showDialog by remember { mutableStateOf(false) }
 
 
     Scaffold(
@@ -69,8 +90,20 @@ fun HomeScreen(
             TopAppBar(
                 title = { Text("Catatan Kuliah") },
                 actions = {
-                    IconButton(onClick = { viewModel.fetchNotes() }) {
-                        Icon(Icons.Default.Refresh, contentDescription = "Refresh")
+                    IconButton(onClick = {
+                        if (user.email.isEmpty()){
+                            CoroutineScope(Dispatchers.IO).launch { signIn(context, dataStore) }
+                        }
+                        else{
+                            showDialog = true
+                        }
+
+                    }) {
+                        Icon (
+                            painter = painterResource(R.drawable.baseline_account_circle_24),
+                            contentDescription = stringResource(R.string.profil),
+                            tint = MaterialTheme.colorScheme.primary
+                        )
                     }
                 }
             )
@@ -100,11 +133,67 @@ fun HomeScreen(
                     modifier = Modifier.align(Alignment.Center)
                 )
                 else -> NoteList(notes = notes, onItemClick = onEditNote)
-
             }
+        }
+        if (showDialog){
+            ProfilDialog(
+                user = user,
+                onDismissRequest = { showDialog = false}) {CoroutineScope(Dispatchers.IO).launch { signOut(context, dataStore) }
+                showDialog= false }
         }
     }
 }
+
+private suspend fun signIn(context: Context, dataStore: UserDataStore) {
+    val googleIdOption: GetGoogleIdOption = GetGoogleIdOption.Builder()
+        .setFilterByAuthorizedAccounts(false)
+        .setServerClientId(BuildConfig.API_KEY)
+        .build()
+
+    val request: GetCredentialRequest = GetCredentialRequest.Builder()
+        .addCredentialOption(googleIdOption)
+        .build()
+
+    try {
+        val credentialManager = CredentialManager.create(context)
+        val result = credentialManager.getCredential(context, request)
+        handleSignIn(result, dataStore)
+    } catch (e: GetCredentialException) {
+        Log.e("SIGN-IN", "Error: ${e.errorMessage}")
+    }
+}
+
+private suspend fun handleSignIn(result: GetCredentialResponse, dataStore: UserDataStore) {
+    val credential = result.credential
+    if (credential is CustomCredential &&
+        credential.type == GoogleIdTokenCredential.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL) {
+        try {
+            val googleId = GoogleIdTokenCredential.createFrom(credential.data)
+            val nama = googleId.displayName ?: ""
+            val email = googleId.id
+            val photoUrl = googleId.profilePictureUri.toString()
+            dataStore.saveData(User(nama, email, photoUrl))
+        } catch (e: GoogleIdTokenParsingException) {
+            Log.e("SIGN-IN", "Error: ${e.message}")
+        }
+    } else {
+        Log.e("SIGN-IN", "Error: unrecognized custom credential type.")
+    }
+}
+
+private suspend fun signOut(context: Context, dataStore: UserDataStore){
+    try{
+        val credentialManager = CredentialManager.create(context)
+        credentialManager.clearCredentialState(
+            ClearCredentialStateRequest()
+        )
+        dataStore.saveData(User())
+    } catch (e: ClearCredentialException) {
+        Log.e("SIGN-IN", "Error: ${e.errorMessage}")
+    }
+}
+
+
 @Composable
 fun NoteList(notes: List<Note>, onItemClick: (Note) -> Unit) {
     LazyColumn(
